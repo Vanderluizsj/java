2c69616d6f7f3707e85c375202faa9338cf9f8f5
Vanderluizsj <48038130+Vanderluizsj@users.noreply.github.com>
2020-05-31 00:50:01 -0300

    Ex2_ContadorDeCliques

--- Ex1_ImprimirLimpar/src/ex1_imprimir/limpar/Main.java
+++ Ex1_ImprimirLimpar/src/ex1_imprimir/limpar/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame Janela1 = new ex1Gui();  //cria o objeto Janela1 do tipo jFrame com nome ex1Gui
-        Janela1.setVisible(true); //
+        Janela1.setVisible(true); //torna a janela grafica visivel
     }
     
 }
---
+++ Ex2_ContadorCliques/src/ex2_contador/cliques/Main.java
@@ -0,0 +1,13 @@
+
+package ex2_contador.cliques;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame janela2 = new ex2Gui();
+        janela2.setVisible(true);
+    }
+    
+}
